1dbc7a18cb40e9f83632c2d74fde31e80430bf8c
gamankr <gamankr@gmail.com>
2026-06-10 03:23:03 +0200

    Add the inference scripts - decoding, top_p sampling

---
+++ inference.py
@@ -0,0 +1,148 @@
+from PIL import Image
+import torch
+import fire
+
+from processing_paligemma import PaliGemmaProcessor
+from modeling_gemma import KVCache, PaliGemmaForConditionalGeneration
+from utils import load_hf_model
+
+def move_inputs_to_device(model_inputs: dict, device: str):
+    model_inputs = {k: v.to(device) for k,v in model_inputs.items()}
+    return model_inputs
+
+def get_model_inputs(processor:PaliGemmaProcessor,
+                     prompt: str,
+                     image_file_path: str,
+                     device: str):
+    image = Image.open(image_file_path)
+    images = [image]
+    prompts = [prompt]
+    model_inputs = processor(text=prompts, images=images)
+    model_inputs = move_inputs_to_device(model_inputs, device)
+    return model_inputs
+
+def _sample_top_p(probs: torch.Tensor, p: float):
+    # [batch_size, vocab_size]
+    probs_sort, probs_idx = torch.sort(probs, dim=-1, descending=True)
+    # [batch_size, vocab_size]
+    probs_sum = torch.cumsum(probs_sort, dim=-1)
+    
+    # Mask is set to True for the probabilities that are not selected by top_p
+    # 
+    # (Substracting "probs_sort" shifts the cumulative sum by 1 position to the right 
+    # before masking - Done to include the probabilities that cross the "top_p" percentage)
+    mask = probs_sum - probs_sort > p
+    # Zero out the probabilities of tokens not selected by top_p
+    probs_sort[mask] = 0.0
+    # Normalize the probabilities so that they sum to 1
+    probs_sort.div_(probs_sort.sum(dim=-1, keepdim=True))
+    # Sample a token (its index) from the top p distribution
+    next_token = torch.multinomial(probs_sort, num_samples=1)
+    # Get the token position in the vocabulary corresponding to the sampled index
+    next_token = torch.gather(probs_idx, -1, next_token) # Extracts value from "probs_idx" at index "next_token"
+    return next_token
+
+def test_inference(
+            model: PaliGemmaForConditionalGeneration,
+            processor: PaliGemmaProcessor,
+            device: str,
+            prompt: str,
+            image_file_path: str,
+            max_tokens_to_generate: int,
+            temperature: float,
+            top_p: float,
+            do_sample: bool
+            ):
+    model_inputs = get_model_inputs(processor, prompt, image_file_path, device)
+    input_ids = model_inputs["input_ids"]
+    attention_mask = model_inputs["attention_mask"]
+    pixel_values = model_inputs["pixel_values"]
+
+    kv_cache = KVCache()
+
+    # Generate tokens until you see the stop token
+    stop_token = processor.tokenizer.eos_token_id
+    generated_tokens = []
+
+    for _ in range(max_tokens_to_generate):
+        # Get the model outputs - First iteration of this 'for' loop will be prefill phase
+        outputs = model(input_ids=input_ids,
+                        pixel_values=pixel_values,
+                        attention_mask=attention_mask,
+                        kv_cache=kv_cache)
+        
+        kv_cache = outputs["kv_cache"]
+        next_token_logits = outputs["logits"][:,-1,:] # logits.shape : [batch_size, seq_len, vocab_size]
+                                                      # Using only the logits of the last token in the sequence
+                                                      # next_token_logits.shape : [batch_size, vocab_size]
+        
+        # Sample the next token
+        if do_sample:
+            # Apply temperature
+            next_token_logits = torch.softmax(next_token_logits / temperature, dim=-1)
+            next_token = _sample_top_p(next_token_logits, top_p)
+        else:
+            next_token = torch.argmax(next_token_logits, dim=-1, keepdim=True)
+        # next_token.shape : [batch_size, 1]
+        assert next_token.size() == (1,1)
+        next_token = next_token.squeeze(0) # remove batch dimension
+        generated_tokens.append(next_token)
+        # Stop if stop token has been generated
+        if next_token.item() == stop_token:
+            break
+        # Append the next_token to the input
+        input_ids = next_token.unsqueeze(-1)
+        # Add a 1 to attention_mask for each new token - No masking in PaliGemma
+        attention_mask = torch.concat([attention_mask, torch.ones((1,1), device=input_ids.device)], dim=-1)
+
+    generated_tokens = torch.concat(generated_tokens, dim=-1)
+    # Decode the generated tokens
+    decoded = processor.tokenizer.decode(generated_tokens, skip_special_tokens=True)
+
+    print(prompt + decoded)                           
+        
+
+def main(
+        model_path: str = None,
+        prompt: str = None,
+        image_file_path: str = None,
+        max_tokens_to_generate: int = 100,
+        temperature: float = 0.8,
+        top_p: float = 0.9,
+        do_sample: bool = False,
+        only_cpu: bool = False,
+):
+    device = "cpu"
+
+    if not only_cpu:
+        if torch.cuda.is_available():
+            device = "cuda"
+        elif torch.backends.mps.is_available(): # For Macbook
+            device = "mps" 
+    
+    print("Device in use: ", device)
+
+    print("Loading Model")
+    model, tokenizer = load_hf_model(model_path, device)
+    model = model.to(device).eval()
+
+    num_image_tokens = model.config.vision_config.num_image_tokens
+    image_size = model.config.vision_config.image_size
+    processor = PaliGemmaProcessor(tokenizer, num_image_tokens, image_size)   
+
+    print("Running Inference")
+    with torch.no_grad():
+        test_inference(
+            model,
+            processor,
+            device,
+            prompt,
+            image_file_path,
+            max_tokens_to_generate,
+            temperature,
+            top_p,
+            do_sample
+        )
+
+if __name__ == "__main__":
+    fire.Fire(main)
